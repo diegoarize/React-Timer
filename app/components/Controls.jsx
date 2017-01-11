ff6eb93ec035aa4dfa1c61b4a42928a97acafc15
diegoarize <diegoarize@outlook.com>
2017-01-11 14:51:27 -0300

    Creating the Timer App

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,8 +2,9 @@ var React = require('react');
 
 var Controls = React.createClass({
   propTypes: {
-    countdownStatus: React.PropTypes.string.isRequired,
-    onStatusChange: React.PropTypes.func.isRequired
+    countdownStatus: React.PropTypes.string,
+    onStatusChange: React.PropTypes.func.isRequired,
+    timerStatus: React.PropTypes.string
   },
   onStatusChange: function (newStatus) {
     return () => {
@@ -11,12 +12,16 @@ var Controls = React.createClass({
     }
   },
   render: function () {
-    var {countdownStatus} = this.props;
+    var {countdownStatus, timerStatus} = this.props;
     var renderStartStopButton = () => {
       if (countdownStatus === 'started') {
         return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
       } else if (countdownStatus === 'paused') {
         return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
+      } else if (timerStatus === 'started') {
+        return <button className="button" onClick={this.onStatusChange('paused')}>Stop</button>
+      } else if (timerStatus === 'paused') {
+        return <button className="button" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
 
